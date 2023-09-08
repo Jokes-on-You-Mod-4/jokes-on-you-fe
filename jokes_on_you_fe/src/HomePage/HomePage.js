bbd6adc24614648a7f49a8d1168e18129842cd44
Joshua Martin <ursa8minority@gmail.com>
2023-09-08 13:07:00 -0600

    upload images to assets

---
+++ jokes_on_you_fe/src/HomePage/HomePage.js
@@ -0,0 +1,32 @@
+import React, { useState } from 'react';
+import './HomePage.css'; 
+import '../images/Smiley face1.png'
+
+function HomePage() {
+  const [selectedCategory, setSelectedCategory] = useState('');
+
+  const handleCategoryChange = (event) => {
+    setSelectedCategory(event.target.value);
+  };
+
+  return (
+    <div className="HomePage">
+      <h1 className="animate-character">Jokes on You</h1>
+      <img src="/Smiley face1.png" alt="Smiley Face" />
+
+      <div className="category-container">
+        {selectedCategory && <p>Selected Category: {selectedCategory}</p>}
+        <label htmlFor="categorySelector">Select a Joke Category:</label>
+        <select
+          id="categorySelector"
+          value={selectedCategory}
+          onChange={handleCategoryChange}
+        >
+          <option value="">-- Select Category --</option>
+        </select>
+      </div>
+    </div>
+  );
+}
+
+export default HomePage;
